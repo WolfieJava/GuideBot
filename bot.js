49de03156f87c42ad62d0f1ed3f30ae3d79e3655
CodeBeautifyGuide <31388169+CodeBeautifyGuide@users.noreply.github.com>
2017-08-28 08:48:52 +0800

    Updated for Part 3

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 // All things with Client or client will not work! Must be bot.
-const prefix = "!";
-// Defines the prefix
+const config = require('./config.json');
+// Defines the config file and let us use it here!
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.username}`)
@@ -10,16 +10,24 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (message) => {
-    if (message.content.startsWith(prefix + 'ping')) {
+    if (message.content.startsWith(config.prefix + 'ping')) {
         message.reply('Pong!');
     } else
     // Make another Statement
-    if (message.content.startsWith(prefix + 'feed')) {
+    if (message.content.startsWith(config.prefix + 'feed')) {
         message.reply(`C'mon give me Some FOOD! :shallow_pan_of_food:`)
+    } else
+    // Make another Statement
+    if (message.content.startsWith(config.prefix + 'shutdown')) {
+        if (message.author.id !== config.ownerID)
+        // Defines your ID on the Config File
+            return message.reply('You must be the Owner! :x:');
+            // If your ID does not match with my ID, it will return the Statement Above
+        message.channel.send('Shutting Down...').then(() => bot.destroy().then(() => process.exit()));
     }
 });
 // Now you need to use the Prefix
 
 
 // Logs in
-bot.login('Your Bot Token')
+bot.login(config.token)
